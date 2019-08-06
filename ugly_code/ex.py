0f244abeae1e1d75e84379f42343f970336d93de
Memory_Leak <irealing@163.com>
2019-08-06 11:58:47 +0800

    feat: 新增JSON读取工具，新增自动配置工具，新增纯文本写入工具

--- ugly_code/ex.py
+++ ugly_code/ex.py
@@ -1,8 +1,9 @@
 """扩展工具模块"""
+import json
 from typing import Any
 
 __author__ = 'Memory_Leak<irealing@163.com>'
-__all__ = ('ObjectProxy',)
+__all__ = ('ObjectProxy', 'json_reader', 'load_json_config', 'text_writer')
 
 
 class ObjectProxy(object):
@@ -32,3 +33,42 @@ class ObjectProxy(object):
 
     def as_dict(self) -> dict:
         return self._raw
+
+
+def json_reader(filename: str):
+    """
+    从文件读取JSON数据
+    :param filename:
+    :return:
+    """
+    with open(filename, 'r', encoding='utf8') as input_stream:
+        content = input_stream.read()
+        return json.loads(content)
+
+
+def load_json_config(env: dict, filename: str):
+    """从JSON加载配置"""
+    cfg = json_reader(filename)
+    if not isinstance(cfg, dict):
+        raise TypeError("json file {} is not dict".format(filename))
+    for k, v in cfg.items():
+        if k.startswith('_'):
+            continue
+        k = k.upper()
+        if k in env:
+            env[k] = v
+
+
+def text_writer(filename: str, coding: str = 'utf-8'):
+    """
+    写入纯文本文档
+    :param filename:
+    :param coding:
+    :return:
+    """
+    with open(filename, 'w', encoding=coding) as output:
+        while True:
+            line = yield
+            if line is None:
+                break
+            output.write(line)
